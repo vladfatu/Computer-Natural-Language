c53357b87e11e12f96a8bb303944a10658ba5950
vlad fatu <colegu123@gmail.com>
2013-04-22 11:24:24 +0300

    -

--- src/com/cnl/conversion/classification/pojo/ClassificationMethod.java
+++ src/com/cnl/conversion/classification/pojo/ClassificationMethod.java
@@ -4,8 +4,28 @@ import java.util.List;
 
 import com.cnl.conversion.data.Data;
 
-public interface ClassificationMethod {
+public abstract class ClassificationMethod {
 	
-	public ClassificationClass getClassificationClass(List<ClassificationClass> classes, Data data, List<String> stems);
+	public ClassificationClass getClassificationClass(List<ClassificationClass> classes, Data data, List<String> stems)
+	{
+		if (classes.size() > 0)
+		{
+			double max = 2;
+			ClassificationClass bestClassificationClass = classes.get(0);
+			for (ClassificationClass classificationClass : classes)
+			{
+				double probability = getProbabilityForClassificationClass(classificationClass, data, stems);
+				if (probability > max || max == 2)
+				{
+					max = probability;
+					bestClassificationClass = classificationClass;
+				}
+			}
+			return bestClassificationClass;
+		}
+		return null;
+	}
+	
+	protected abstract double getProbabilityForClassificationClass(ClassificationClass classificationClass, Data data, List<String> stems);
 
 }
