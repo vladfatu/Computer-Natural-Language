6e151e13a44373b19ce58562cbd2a3307050fc75
vlad <colegu123@gmail.com>
2013-04-26 03:43:21 +0300

    -

--- src/com/cnl/conversion/Main.java
+++ src/com/cnl/conversion/Main.java
@@ -1,52 +1,47 @@
 package com.cnl.conversion;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.cnl.conversion.data.WsdData;
-import com.cnl.conversion.wsd.pojo.Word;
-import com.cnl.conversion.wsd.pojo.WsdMethod;
-import com.cnl.conversion.wsd.supervised.WsdSupervised;
-
-import edu.mit.jwi.item.POS;
+import com.cnl.conversion.classification.NaiveBayes.NaiveBayes;
+import com.cnl.conversion.classification.pojo.Classification;
+import com.cnl.conversion.classification.sentiment.SentimentClassification;
+import com.cnl.conversion.data.SentimentData;
 
 public class Main {
 	
 	public static void main(String[] args)
 	{
 		//Sentiment analasys
-//		Classification setimentClassification = new SentimentClassification(new SentimentData(), new NaiveBayes());
-//		setimentClassification.trainAndTest();
+		Classification setimentClassification = new SentimentClassification(new SentimentData(), new NaiveBayes());
+		setimentClassification.trainAndTest();
 		
 		//WSD
 		
-		WsdData wsdData = new WsdData();
-		
-		Word line = new Word("line", POS.NOUN);
-		
-		WsdMethod wsdMethod = new WsdSupervised();
-		
-		wsdMethod.test(line, wsdData.getContexts());
-		
-		
-		List<Word> featureWords = new ArrayList<Word>();
-		Word featureWord = new Word("mooring", POS.NOUN);
-		featureWords.add(featureWord);
-		
-		featureWord = new Word("seabed", POS.NOUN);
-		featureWords.add(featureWord);
-		
-		featureWord = new Word("steel", POS.NOUN);
-		featureWords.add(featureWord);
-		
-		featureWord = new Word("design", POS.NOUN);
-		featureWords.add(featureWord);
-		
-		featureWord = new Word("platform", POS.NOUN);
-		featureWords.add(featureWord);
-		
-		featureWord = new Word("structure", POS.NOUN);
-		featureWords.add(featureWord);
+//		WsdData wsdData = new WsdData();
+//		
+//		Word line = new Word("line", POS.NOUN);
+//		
+//		WsdMethod wsdMethod = new WsdSupervised();
+//		
+//		wsdMethod.test(line, wsdData.getContexts());
+//		
+//		
+//		List<Word> featureWords = new ArrayList<Word>();
+//		Word featureWord = new Word("mooring", POS.NOUN);
+//		featureWords.add(featureWord);
+//		
+//		featureWord = new Word("seabed", POS.NOUN);
+//		featureWords.add(featureWord);
+//		
+//		featureWord = new Word("steel", POS.NOUN);
+//		featureWords.add(featureWord);
+//		
+//		featureWord = new Word("design", POS.NOUN);
+//		featureWords.add(featureWord);
+//		
+//		featureWord = new Word("platform", POS.NOUN);
+//		featureWords.add(featureWord);
+//		
+//		featureWord = new Word("structure", POS.NOUN);
+//		featureWords.add(featureWord);
 		
 //		long t = System.currentTimeMillis();
 //		
